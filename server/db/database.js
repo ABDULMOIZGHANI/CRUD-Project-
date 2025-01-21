7ecd38120483f01bc5acf312caf060a8c8df64ce
ABDUL MOIZ <babamoiz289@gmail.com>
2025-01-21 19:01:24 +0500

    hide password

--- server/db/database.js
+++ server/db/database.js
@@ -3,7 +3,7 @@ import mongoose, { mongo } from "mongoose";
 const connectDB = async () => {
   try {
     const databaseInstance = await mongoose.connect(
-      "mongodb+srv://abdulmoiz:moizmoiz123@cluster0.g5spt.mongodb.net/CRUD_APP"
+      "mongodb+srv://abdulmoiz:YOUR_DATABSE_ATLAS_PASSWORD@cluster0.g5spt.mongodb.net/CRUD_APP"
     );
     console.log(
       `Mongo DB Connected successfully at PORT ${databaseInstance.connection.port} HOST:${databaseInstance.connection.host}`
